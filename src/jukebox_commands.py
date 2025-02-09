7f0186452fa8c1aa6c657fccb333cb3964f070ac
blueberry <dearbblueberry@gmail.com>
2025-02-09 12:13:30 +1100

    Update Versions command for deprecated pkg_resources module.

--- src/jukebox_commands.py
+++ src/jukebox_commands.py
@@ -32,7 +32,6 @@ import json
 import os
 from io import StringIO
 
-# import pkg_resources
 import random
 import re
 import sys
@@ -40,6 +39,7 @@ from datetime import date, datetime
 from importlib import reload
 from importlib import metadata
 from math import ceil, floor
+from packaging.version import Version
 from typing import List, Dict, Union, Optional, Any, Tuple
 from urllib import request
 
@@ -1264,41 +1264,51 @@ class Commands(commands.Cog, name=config.COG_COMMANDS):
 
         await ctx.reply(embed=embed)
 
-    # @commands.command(name="versions", aliases=["version", "vers", "ver"], hidden=True)
-    # @commands.check(is_admin)
-    # async def send_versions(self, ctx: Context) -> None:
-    #     msg: str
-    #     embed: discord.Embed = discord.Embed(colour=get_embed_colour(ctx.guild))
+    @commands.command(name="versions", aliases=["version", "vers", "ver"], hidden=True)
+    @commands.check(is_admin)
+    async def send_versions(self, ctx: Context) -> None:
+        msg: str
+        embed: discord.Embed = discord.Embed(colour=get_embed_colour(ctx.guild))
 
-    #     # List major packages with additional information and source links
-    #     embed.description = strings.get("info_package_major")
-    #     embed.set_footer(text="Python " + sys.version)
-    #     packages: dict = pkg_resources.working_set.by_key
-    #     for key in config.PACKAGE_CHECKS:
-    #         meta_dict: dict = dict(metadata.metadata(key))
-    #         url = f'https://pypi.python.org/pypi/{key}/json'
-    #         releases = json.loads(request.urlopen(url).read())['releases']
-    #         msg = strings.get("info_package_major_entry").format(
-    #             meta_dict["Version"],
-    #             meta_dict["Summary"],
-    #             meta_dict["Home-page"])
-    #         release_key: str = list(releases.keys())[-1]
-    #         if release_key != meta_dict["Version"]:
-    #             msg += strings.get("info_package_update").format(
-    #                 strings.emoji_star,
-    #                 release_key,
-    #                 re.split(r"([a-z])", releases[release_key][-1]["upload_time"], 1, flags=re.I)[0]
-    #             )
-    #         embed.add_field(name=meta_dict["Name"], value=msg, inline=False)
+        # List major packages with additional information and source links
+        embed.description = strings.get("info_package_major")
+        embed.set_footer(text="Python " + sys.version)
+        packages: dict = {d.name: d for d in metadata.distributions()}
+        for key in config.PACKAGE_CHECKS:
+            meta_dict: dict = dict(metadata.metadata(key))
+            url = f'https://pypi.python.org/pypi/{key}/json'
+            releases = json.loads(request.urlopen(url).read())['releases']
+            msg = strings.get("info_package_major_entry").format(
+                meta_dict["Version"],
+                meta_dict["Summary"],
+                meta_dict["Project-URL"])
+            semver_current: Version = Version(meta_dict["Version"])
+            for release_key in reversed(releases.keys()):
+                # Append update notice for newer versions of major packages
+                semver_release: Version = Version(release_key)
+                # Ignore unstable releases
+                ignore: bool = semver_release.is_prerelease or semver_release.is_devrelease
+                if semver_release > semver_current:
+                    if not ignore:
+                        msg += strings.get("info_package_update").format(
+                            strings.emoji_star,
+                            release_key,
+                            re.split(r"([a-z])", releases[release_key][-1]["upload_time"], 1, flags=re.I)[0]
+                        )
+                        break
+                else:
+                    # Ignore older versions
+                    break
+            embed.add_field(name=meta_dict["Name"], value=msg, inline=False)
 
-    #     # List minor packages with minimal name and version information
-    #     embed.add_field(name=strings.get("info_package_minor"), value="  ".join([
-    #         strings.get("info_package_minor_entry").format(key, packages[key].version)
-    #         for key in packages
-    #         if key not in config.PACKAGE_CHECKS
-    #     ]))
+        # List minor packages with minimal name and version information
+        embed.add_field(name=strings.get("info_package_minor"), value="  ".join([
+            strings.get("info_package_minor_entry").format(key, packages[key].version)
+            for key in packages
+            if key not in config.PACKAGE_CHECKS
+        ]))
 
-    #     await ctx.reply(embed=embed)
+        await ctx.reply(embed=embed)
 
     @commands.command(name="logs", aliases=["log"], hidden=True)
     @commands.check(is_admin)
